4bfe6a720921d28ba5c5d49339ccc716d552615c
Matheus Martins <martinsjpa3@gmail.com>
2023-07-17 22:11:51 -0300

    feature: desenvolvimento parcial AuthController / AuthService

---
+++ src/Application/CefetPark.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using CefetPark.Application.ViewModels.Request.Auth.Post;
+
+namespace CefetPark.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        public Task<string> LoginAsync(LoginPostRequestAuth request);
+    }
+}
---
+++ src/WebApi/CefetPark.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using CefetPark.Application.Interfaces.Services;
+using CefetPark.Application.ViewModels.Request.Auth.Post;
+using CefetPark.Utils.Interfaces.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CefetPark.WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : PrincipalController
+    {
+        private readonly INotificador _notificador;
+        private readonly IAuthService _authService;
+        public AuthController(INotificador notificador, IAuthService authService) : base(notificador)
+        {
+            _notificador = notificador;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LoginAsync(LoginPostRequestAuth request)
+        {
+            var response = await _authService.LoginAsync(request);
+            return CustomResponse(response);
+        }
+    }
+}
